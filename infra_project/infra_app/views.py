97b6445525041c54ad8304e3fc0c94f469a39b35
Андрей Абрамов <89194795009@mail.ru>
2023-04-02 14:26:22 +0500

    меняю фразу в views

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось! Ура! Я уже почти супер-пупер программист!!!!!!!!!!!!!!!!!')
 
 
 def second_page(request):
